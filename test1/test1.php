c86a912853be1e40d21becbe85be0c77ae49deae
Helfrench02 <napster_helfrench@yahoo.com>
2022-02-02 21:18:30 +0800

    DoneTest1

--- test1/test1.php
+++ test1/test1.php
@@ -1,98 +1,63 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <H1> hELLO WORLD</H1>
-</body>
-</html>
-
-
-<?php
 <?php
 
-// $string = file_get_contents("test.json");
-// if ($string === false) {
-//     // deal with error...
-// }
+class Json 
+{
 
-// $json_a = json_decode($string, true);
-// if ($json_a === null) {
-//     // deal with error...
-// }
-
-// foreach ($json_a as $person_name => $person_a) {
-//     echo $person_a['status'];
-// }
-
-
-// $string = file_get_contents("test.json");
-// $json_a = json_decode($string, true);
-// echo "<ul>";
-// foreach($json_a['books'] as $movies){
-//     echo $movies['title']."<br>";
-//     echo $movies['publish']."<br>";
-//     echo $movies['author']."<br>";
-//     echo "<br>";
-
-    
-// }
-// print_r($json_a);
-
-
-
-//2 ways
-  //this is for string from $_REQUEST,$_POST to array
-//   $jsonText = $_REQUEST['myJSON'];
-//   $decodedText = html_entity_decode($jsonText);
-//   $myArray = json_decode($decodedText, true);
-  
-//   //this is for json to array
-//   $assosiative_array = json_decode(json_encode($jsonText),true);
-
-
- class Json {
-
-    public $json;
     public $result;
+    public $dataType;  
 
-   
-
-    function getFile()
+    function __construct($dataType)
     {
-        $answer = scandir('parseJsonToArray');
+
+    $this->dataType = $dataType;                             
         
-            echo var_dump($answer);
-         
-        // $extension = '.json';
-        // $result = file_get_contents($jsonfile.$extension);
-      
+    if(!file_exists('sample4.json'))
+        {
+            echo "No File Found inside the folder";
+            die("No such File");
+            return;
+        }else
+        { 
+            $this->hasFile = true;
+            return  $this->result = file_get_contents('sample4.json');  
+        }  
+
+    }  
+
+    function ArrayToString():string
+    {
+
+        $encodeData = JSON_encode($this->result);
+        return $encodeData;
+        
     }
 
-    // function parseJson{
-    //     $decodedData = json_decode($result, true);
+    function ObjectToArray(): array 
+    {                  
+        $decodedData = json_decode($this->result, true);    
+        return $decodedData;                       
+    
+    }
 
-    //     if($dataType == "Array")
-    //     {
-    //         var_dump($decodedData);
-    //     }
-    //     if ($dataType == "String")
-    //     {
-    //         foreach($decodedData['books'] as $movies)
-    //         {
-    //                 echo $movies['title']."<br>";
-    //                 echo $movies['publish']."<br>";
-    //                 echo $movies['author']."<br>";
-    //                 echo "<br>";
-    //         } 
-    //     }
-    // }
+    function parseJson()
+    {
+    if ($this->dataType == "1")  
+        {
+            echo $this->ArrayToString();
+        }elseif($this->dataType == "2")
+        {
+            echo "<pre>";
+            print_r($this->ObjectToArray());
+            echo "</pre>";
+        } 
+    } 
+
 }
 
-$test1 = new Json();
-echo $test1->getFile();
+
+// 1 = Array to String
+// 2 = String to Array
+$test1 = new Json("1");
+$test1->parseJson();
 ?>
+
